f4e9d7cec29faa048d4c45e26e74971ae59d8045
Craig Rufener <crufener@outlook.com>
2016-09-29 01:24:05 -0700

    Adding the missingNumber and homework folders

--- classes.js
+++ classes.js
@@ -5,11 +5,44 @@ var products = [
     { name: 'Grapes', price: 4.00 }
 ];
 var expensiveProducts = products.filter(function (product) { return product.price > 5.00; });
-console.log(expensiveProducts);
 var criteria = function (product) { return product.price > 5.00; };
 var expensiveProducts1 = products.filter(criteria);
-console.log(expensiveProducts1);
 var total = products.reduce(function (s, c) {
     return s + c.price;
 }, 0);
-console.log(total);
+addListItem('template literals', 'All about temlate literals\ncool!');
+var message = "This is a long message that\nwraps across multiple lines. You could not\ndo this without using a template literal.";
+addListItem("randome message", message);
+function addListItem(msg, msg2) {
+    var ul = document.getElementById('ul');
+    var div = document.getElementById("div");
+    var p = document.createElement('div');
+    var li = document.createElement('LI');
+    var text = li.innerHTML = msg;
+    var ptext = p.innerHTML = msg2;
+    ul.appendChild(li);
+    div.appendChild(p);
+}
+var Product = (function () {
+    function Product() {
+    }
+    Product.prototype.calculateTax = function () {
+        return this.price * 0.08;
+    };
+    return Product;
+}());
+;
+var product1 = new Product();
+product1.name = "Eggs";
+product1.price = 1.25;
+var Person = (function () {
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Person.prototype.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
+    return Person;
+}());
+var craig = new Person("Craig", "Rufener");
--- classes.ts
+++ classes.ts
@@ -6,14 +6,71 @@ let products = [
 ];
 
 let expensiveProducts = products.filter(product => product.price > 5.00);
-console.log(expensiveProducts);
+//console.log(expensiveProducts);
 
 let criteria = product => product.price > 5.00;
 
 let expensiveProducts1 = products.filter(criteria);
-console.log(expensiveProducts1);
+//console.log(expensiveProducts1);
 
 let total = products.reduce((s, c) => {
-	return s + c.price;
+    return s + c.price;
 }, 0);
-console.log(total);
+//console.log(total);
+
+/*TEMPLATE LITERALS*/
+addListItem('template literals', 'All about temlate literals\ncool!');
+let message = `This is a long message that
+wraps across multiple lines. You could not
+do this without using a template literal.`;
+addListItem(`randome message`, message);
+
+/*ADD NEW TOPICS COVERD TO THE LIST ON THE  INDEX.HTLM PAGE AND A SECOND MESSAGE AS A PARAGRAPH*/
+/** addListItem : (string , string) -> (HTML list item, Html p) */
+function addListItem(msg: string, msg2: string): void {
+    let ul = document.getElementById('ul');
+    let div = document.getElementById("div");
+    let p = document.createElement('div');
+    let li = document.createElement('LI');
+    let text = li.innerHTML = msg;
+    let ptext = p.innerHTML = msg2;
+    ul.appendChild(li);
+    div.appendChild(p);
+}
+
+
+//CLASSES
+
+/** class Product
+    property name: string
+    property price: number
+    method calculateTax : number
+**/
+class Product {
+    name: string;
+    price: number;
+    calculateTax() {
+        return this.price * 0.08;
+    }
+};
+
+/*    Make an insance of the Product class  */
+/**product1 : class Product
+    property name : string
+    property price : number
+    **/
+let product1 = new Product();
+product1.name = "Eggs";
+product1.price = 1.25;
+//console.log(product1.calculateTax());
+
+/*    Better way to make a class.  put fields in the constructor paramaters */
+class Person {
+    constructor(public firstName: string, public lastName: string) { }
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+/*    Make an insance of the Person class.
+        Notice the property fields are put in the call to Person class*/
+let craig = new Person("Craig", "Rufener");
